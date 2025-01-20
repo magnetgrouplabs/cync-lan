8fa04c07fc0b67a4e97f8469dfab68a83f66ca40
baudneo <86508179+baudneo@users.noreply.github.com>
2025-01-20 08:36:06 -0700

    refactor type_2_str

--- src/cync-lan.py
+++ src/cync-lan.py
@@ -638,19 +638,23 @@ class CyncCloudAPI:
 
 type_2_str = {
     31: "C by GE Full Color A19 Bulb (BTLE only)",
-    37: "Dimmer Switch with Motion and Ambient Light - Neutral Required",
-    42: "Reveal HD+ Full Color Under Cabinet Light - 18 Inch",
-    43: "Reveal HD+ Full Color Under Cabinet Light - 24 Inch",
-    55: "Dimmer Switch - No Neutral",
-    68: "Indoor Direct Connect Plug",
+
+    37: "Direct Connect Dimmer Switch with Motion and Ambient Light - Neutral Required",
+    42: "Reveal HD+ Full Color Direct Connect Under Cabinet Light - 18 Inch",
+    43: "Reveal HD+ Full Color Direct Connect Under Cabinet Light - 24 Inch",
+    55: "Direct Connect Dimmer Switch - No Neutral",
+
+    68: "Direct Connect Indoor Plug",
+
     113: "Wire-Free Dimmer with White Temperature Switch (BTLE only)",
+
     133: "Full Color Direct Connect LED Light Strip Controller",
     137: "Full Color Direct Connect A19 Bulb",
     138: "Full Color Direct Connect BR30 Floodlight",
     140: "Full Color Direct Connect Outdoor PAR38 Floodlight",
     146: "Full Color Direct Connect Edison ST19 Bulb",
     147: "Full Color Direct Connect Edison G25 Bulb",
-    148: "Dimmable Direct Connect Edison ST19 Bulb",
+    148: "Direct Connect Dimmable White (2700K) Edison ST19 Bulb",
     152: "Reveal HD+ White (2700K) A19 Bulb",
 
     224: "Direct Connect Thermostat",
