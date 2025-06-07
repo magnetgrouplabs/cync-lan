c35ea845bd9d6b22fda5a2b05d82a39cb7a4997d
baudneo <86508179+baudneo@users.noreply.github.com>
2025-06-07 15:45:52 -0600

    change to actual location

--- src/cync_lan/const.py
+++ src/cync_lan/const.py
@@ -80,10 +80,10 @@ CYNC_RAW = os.environ.get("CYNC_RAW_DEBUG", "0").casefold() in YES_ANSWER
 CYNC_DEBUG = os.environ.get("CYNC_DEBUG", "0").casefold() in YES_ANSWER
 
 # FIXME: change when done local testing
-# CYNC_BASE_DIR: str = "/root"
-CYNC_BASE_DIR: str = "/opt"
-# CYNC_STATIC_DIR: str = "/root/cync-lan/static"
-CYNC_STATIC_DIR: str = "~/PycharmProjects/cync-lan-addon/cync-lan/static"
+CYNC_BASE_DIR: str = "/root"
+# CYNC_BASE_DIR: str = "/opt"
+CYNC_STATIC_DIR: str = "/root/cync-lan/static"
+# CYNC_STATIC_DIR: str = "~/PycharmProjects/cync-lan-addon/cync-lan/static"
 
 CYNC_CONFIG_FILE_PATH: str = f"{CYNC_BASE_DIR}/cync-lan/config/cync_mesh.yaml"
 CYNC_CLOUD_AUTH_PATH: str = f"{CYNC_BASE_DIR}/cync-lan/config/.cloud_auth.yaml"
